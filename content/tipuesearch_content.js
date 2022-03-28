570af3eb8131783851dc20960ffd909b12fb3002
40923134 <40923134@gm.nfu.edu.tw>
2022-03-29 02:58:21 +0800

    cd2022

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'NX_video', 'text': '', 'tags': '', 'url': 'NX_video.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '學號:40923134 \n 網站: https://40923134.github.io/cd2022/content/index.html \n 倉儲: https://github.com/40923134/cd2022 \n', 'tags': '', 'url': 'About.html'}, {'title': 'NX_video', 'text': '7 _Expressions (Continued) \n \n \n \n \n   \n \n \n \n \n \n \n \n', 'tags': '', 'url': 'NX_video.html'}]};
